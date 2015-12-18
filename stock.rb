da61f7268a064f9652164fafcf41918f69e36001
Harry Oliver <someonecancode@yahoo.co.uk>
2015-12-18 18:00:15 +0000

    Add price in cents

--- stock.rb
+++ stock.rb
@@ -1,9 +1,18 @@
 class BookInStock
-  attr_reader :isbn, :price
+  attr_reader :isbn
+  attr_accessor :price
   
   def initialize(isbn, price)
     @isbn = isbn
     @price = Float(price)
+  end
+  
+  def price_in_cents
+    Integer(price*100 + 0.5) # Round up floating point value
+  end
+  
+  def price_in_cents=(cents)
+    @price = cents / 100.0
   end
   
   def to_s
@@ -18,3 +27,7 @@ b3 = BookInStock.new("isbn3", "5.67")
 puts b1
 puts b2
 puts b3
+puts "Price in cents = #{b3.price_in_cents}"
+b3.price_in_cents = 1234
+puts "b3 price = #{b3.price}"
+puts "b3 in cents again = #{b3.price_in_cents}"
